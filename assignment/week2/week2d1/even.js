102ead4f642d8c5bc333a72718955775408a8df0
Naresh8460 <mudaliar.naresh@gmail.com>
2019-03-23 23:40:38 +0530

    -A

---
+++ assignment/week2/week2d1/even.js
@@ -0,0 +1,5 @@
+//even number
+for (let i=100; i>=2; i--){
+    if(i % 2==0);
+    console.log(i) 
+}
---
+++ assignment/week2/week2d2/5th.js
@@ -0,0 +1,6 @@
+//5th table
+for (i = 1; i <= 10; i++){
+    number=5 
+    result=(i*5)
+   console.log(number + '*' + i + '=' + result)
+   }
